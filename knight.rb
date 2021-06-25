bf9b5a1146cd7914385e058e10f2cf56fda5c2b5
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-06-24 22:48:54 -0700

    Add method to list possible moves knight can make in one move

--- knight.rb
+++ knight.rb
@@ -4,4 +4,27 @@ class Knight
   def initialize
     @location = nil
   end
+
+  def possible_moves(location)
+    x = location[0]
+    y = location[1]
+
+    moves = [
+      [x + 1, y + 2],
+      [x + 2, y + 1],
+      [x + 2, y - 1],
+      [x + 1, y - 2],
+      [x - 1, y - 2],
+      [x - 2, y - 1],
+      [x - 2, y + 1],
+      [x - 1, y + 2]
+    ]
+
+    moves.select do |move|
+      x = move[0]
+      y = move[1]
+
+      (0..7).include?(x) && (0..7).include?(y)
+    end
+  end
 end
