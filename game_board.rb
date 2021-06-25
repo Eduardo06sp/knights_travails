e67069844ffaec9fadce94265a37c37652789471
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-06-24 21:39:51 -0700

    Generate all game board spaces

--- game_board.rb
+++ game_board.rb
@@ -4,4 +4,27 @@ class GameBoard
   def intialize
     @spaces = create_board
   end
+
+  def create_board
+    start = [0, 0]
+    finish = [7, 7]
+    spaces = []
+
+    x = 0
+    y = 0
+
+    until x > 7
+      until y > 7
+        current = [x, y]
+
+        spaces.push(current)
+        y += 1
+      end
+
+      x += 1
+      y = 0
+    end
+
+    spaces
+  end
 end
