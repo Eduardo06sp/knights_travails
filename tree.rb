fd6835eaf64ba6f1a0b2bd0a453ba5aadb54ae26
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-06-27 16:36:48 -0700

    Pass instance of GameBoard as argument to build_tree method

--- tree.rb
+++ tree.rb
@@ -4,11 +4,11 @@ require_relative 'game_board'
 require_relative 'node'
 
 class Tree
-  def initialize
-    @root = build_tree
+  def initialize(board)
+    @root = build_tree(board)
   end
 
-  def build_tree
+  def build_tree(board)
     start = gets.chomp
 
     until start.match?(/\[[0-7], [0-7]\]/)
