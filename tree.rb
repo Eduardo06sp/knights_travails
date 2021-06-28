1cffcffc3e9e5ec0b4373fcb76a2a9dd4dfdbcde
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-06-27 20:47:39 -0700

    Recursively create all possible moves from starting point

--- tree.rb
+++ tree.rb
@@ -24,10 +24,49 @@ class Tree
     create_branches(board, start_index, possible_moves)
   end
 
-  def create_branches(board, start_index, possible_moves, predecessor = nil)
-    root = Node.new(board.spaces[start_index], possible_moves[start_index])
+  def create_branches(board, start_index, possible_moves, predecessor = nil, visited = [])
+    return if visited.include?(board.spaces[start_index])
 
-    p root.value
+    root = Node.new(board.spaces[start_index])
+    root.predecessor = predecessor
+
+    visited.push(root.value)
+
+    current_possible_moves = possible_moves[start_index]
+
+    if current_possible_moves[0]
+      root.move_one = create_branches(board, board.spaces.index(current_possible_moves[0]), possible_moves, root, visited)
+    end
+
+    if current_possible_moves[1]
+      root.move_two = create_branches(board, board.spaces.index(current_possible_moves[1]), possible_moves, root, visited)
+    end
+
+    if current_possible_moves[2]
+      root.move_three = create_branches(board, board.spaces.index(current_possible_moves[2]), possible_moves, root, visited)
+    end
+
+    if current_possible_moves[3]
+      root.move_four = create_branches(board, board.spaces.index(current_possible_moves[3]), possible_moves, root, visited)
+    end
+
+    if current_possible_moves[4]
+      root.move_five = create_branches(board, board.spaces.index(current_possible_moves[4]), possible_moves, root, visited)
+    end
+
+    if current_possible_moves[5]
+      root.move_six = create_branches(board, board.spaces.index(current_possible_moves[5]), possible_moves, root, visited)
+    end
+
+    if current_possible_moves[6]
+      root.move_seven = create_branches(board, board.spaces.index(current_possible_moves[6]), possible_moves, root, visited)
+    end
+
+    if current_possible_moves[7]
+      root.move_eight = create_branches(board, board.spaces.index(current_possible_moves[7]), possible_moves, root, visited)
+    end
+
+    root
   end
 end
 
